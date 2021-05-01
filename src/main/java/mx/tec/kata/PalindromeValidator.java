6d1969f4c287ae9d35f17399e45647d6d9dcbd7f
Javinator11 <sosa.puga.javier@gmail.com>
2021-05-01 14:25:16 -0600

    Step 5

--- src/main/java/mx/tec/kata/PalindromeValidator.java
+++ src/main/java/mx/tec/kata/PalindromeValidator.java
@@ -2,8 +2,9 @@ package mx.tec.kata;
 
 public class PalindromeValidator {
 	public boolean validate(String string) {
-		for (int i = 0; i < string.length() / 2; i++) {
-			if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+		String cleanedString = string.replaceAll("\\s+", "");
+		for (int i = 0; i < cleanedString.length() / 2; i++) {
+			if (cleanedString.charAt(i) != cleanedString.charAt(cleanedString.length() - 1 - i)) {
 				return false;
 			}
 		}
